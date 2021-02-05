1e1ba5f1fd912058c9a57233fa1d91d255864ee2
hba0002 <hba0002@mix.wvu.edu>
2021-02-05 10:34:23 -0500

    About Page

---
+++ index.php
@@ -0,0 +1,60 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>
+        A diverse group of real vampires and witches here to critique your favorite supernatural movies and TV shows. 
+        We won't hold back in pointing out logical and technical flaws of some of the most popular depictions of the supernatural. 
+        </p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card"> 
+                <p class="c"> Opinionated with a touch of sarcasm. What kind of vampire glows? Im looking at you Edward </p>
+                <img src="images/Damon.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p class="c1">Damon Salvatore</p>
+                    <p class="c1">Email: thehotterbrother@gmail.com</p>
+                   
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card"> 
+                <p class= "c">An old school vamp. No human blood for me, I'm on an animal diet </p>
+                <img src="images/Stefan.jpg">
+                <div class="container">
+                    <h2>Finance Manager</h2>
+                    <p class="c2">Stefan Salvatore</p>
+                    <p class= "c2">Email: theripper@hotmail.com</p>
+                   
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+            <p class= "c">I may be pretty, but don't mistake that for weakness. These fangs hurt </p>
+                <img src="images/Caroline.jpg">
+                <div class="container">
+                    <h2>Fashion Expert</h2>
+                    <p class="c3">Caroline Forbes</p>
+                    <p class="c3">Email: vampirebarbie92@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+            <p class= "c">The most powerful witch around. Constantly saving vampires from their own mess</p>
+                <img src="images/Bonnie.jpg">
+                <div class="container">
+                    <h2>Executive Manager</h2>
+                    <p class="c4">Bonnie Bennett</p>
+                    <p class="c4">Email: bonbon@yahoo.com</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</main>
