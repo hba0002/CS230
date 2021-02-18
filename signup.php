564b2f622475203a51360b3341148bc300bf7056
hba0002 <hba0002@mix.wvu.edu>
2021-02-18 17:40:02 -0500

    Login

--- signup.php
+++ signup.php
@@ -8,12 +8,9 @@ require "includes/header.php"
         <div class="h-100 container center-me">
             <div class="my-auto">
                 <div class="signup-form">
-
                     <form action="includes/signup-helper.php" method="post">
-
                         <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
                         <p class="hint.text">Create your account!</p>
-                        <div class="form-group">
                             <div class="row">
                                 <div class="col">
                                     <input type="text"  class="form-control" name="fname" placeholder="First Name" required autofocus>
@@ -22,9 +19,10 @@ require "includes/header.php"
                                     <input type="text" class="form-control" name="lname" placeholder="Last Name" required autofocus>
                                 </div>
                             </div>
-                        </div>
+                        
+                        <div class="form-group">
                         <input type="text" class="form-control" name="uname" placeholder="Username" required autofocus>
-
+                        </div>
                         <label for="inputEmail" class="sr-only">Email address</label>
                         <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required
                             autofocus>
@@ -33,8 +31,7 @@ require "includes/header.php"
                         <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required>
 
                         <label for="inputPassword" class="sr-only">Confirm Password</label>
-                        <input type="password" id="inputPassword" class="form-control" name="con-pwd" placeholder="Confirm Password"
-                            required>
+                        <input type="password" id="inputPassword" class="form-control" name="con-pwd" placeholder="Confirm Password" required>
 
                         <button class="btn btn-lg btn-outline-danger btn-block" name="signup-submit" type="submit">Sign in</button>
                         <p class="mt-5 mb-3 text-muted">&copy; 2020-9999</p>
